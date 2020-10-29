af868cdf65b874a776a087447213ad32f6b93f03
Anisha James <73602224+anishajames@users.noreply.github.com>
2020-10-29 23:17:20 +0000

    Source code added for Exercise1

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -1,5 +1,10 @@
 package com.techreturners.exercise001;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.ListIterator;
+
 public class Exercise001 {
 
     // In this Kata, you will be given an array of numbers in which two numbers occur once and the rest occur only twice. 
@@ -12,6 +17,28 @@ public class Exercise001 {
 
     public int singles(int [] arr){
         // Add your code here!
-        return 0;
+    	
+    	int sum = 0;
+    	int currVal = 0;
+    	//int tracker = 0;
+    	List<Integer> intList = new ArrayList<Integer>(arr.length);
+    	for (int i : arr)
+    	{
+    	    intList.add(i);
+    	}        
+        //System.out.println(list);
+    	int occurence = 0;
+    	ListIterator<Integer> listIterator = intList.listIterator();
+   	 
+    	while(listIterator.hasNext()) {
+    		currVal = listIterator.next();
+    		occurence = Collections.frequency(intList, currVal);
+    		if (occurence == 1) 
+    				sum = sum + currVal;
+    	   // System.out.println(listIterator.next());
+    	}
+    	System.out.println(sum);		
+    	
+        return sum;
     }
 }
